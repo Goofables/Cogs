fb17adb49e8f8bb212bd9f5fde7b10539d9ec074
Goofables <goofables@gmail.com>
2017-10-21 02:09:54 -0600

    Antided working! Watches and deletes with regex along with commands to search for and delete.

--- antided/antided.py
+++ antided/antided.py
@@ -1,55 +1,144 @@
+# -*- coding: utf-8 -*-
 import discord
 from discord.ext import commands
-from .utils.dataIO import dataIO
+from time import sleep
 from .utils import checks
 import asyncio
+import re
 import os
 
 class Antided:
     """Deletes all `ded` messages."""
-    
     def __init__(self, bot):
         self.bot = bot
-        self.settings = dataIO.load_json("data/Antided/settings.json")
-        self.channels = dataIO.load_json("data/Antided/channels.json")
-        
-    @commands.command(pass_context=True)
-    @checks.admin_or_permissions(manage_server=True)
-    async def toggleded(self, ctx):
-        """Toggles antided in current channel"""
-        channel = ctx.message.channel
-        
-    @commands.command(pass_context=True, no_pm=True)
-    @checks.admin_or_permissions(manage_server=True)
-    async def antided(self, ctx):
+        self.regex = re.compile("^([\W_]*d[\W_]*e[\W_]*d[\W_]*)+$")
+
+    @commands.group(pass_context=True)
+    @checks.mod_or_permissions(manage_messages=True)
+    async def dednuker(self, ctx):
+        """Finds all `ded` messages"""
+        if ctx.invoked_subcommand is None:
+            await ctx.invoke(self.channel_dednuker, ctx.message.channel, False)
+
+    @dednuker.command(name="channel", pass_context=True)
+    @checks.mod_or_permissions(manage_messages=True)
+    async def channel_dednuker(self, ctx, delete: str = False, channel: discord.Channel = None, user: discord.User = None):
+        """Finds all `ded` messages in a channel."""
+        if channel is None:
+            channel = ctx.message.channel
+        total = {"messages" : 0, "msg" : 0}
+        msg = "```"
+        async for message in self.bot.logs_from(channel, limit=10000000, before=None):
+            total["messages"] += 1
+            if user and not message.author == user:
+                continue
+            if self.regex.match(message.content.lower()):
+                total["msg"] += 1
+                msg += "\n{} : {} >> {}".format(message.timestamp, message.id, message.author)
+                await self.bot.add_reaction(message, "👎")
+                if delete:
+                    try:
+                        await self.bot.delete_message(message)
+                    except discord.Forbidden:
+                        pass
+        msg += "```"
+        if not msg == "``````":
+            await self.bot.send_message(ctx.message.channel, "Channel: {} Messages: `{}`\n{}".format(channel.mention, total["msg"], msg))
+        msg = "Done!"
+        msg += "\nScanned `{}` messages in this channels.".format(total["messages"])
+        msg += "\nFound `{}` matches.".format(total["msg"])
+        await self.bot.send_message(ctx.message.channel, msg)
+
+    
+    @dednuker.command(name="server", pass_context=True)
+    @checks.admin_or_permissions(administrator=True)
+    async def server_dednuker(self, ctx, delete: str = False, user: discord.User = None):
         server = ctx.message.server
-        message = "Welcome channels:"
-        for channel in self.channels:
+        total = {"messages" : 0, "channels" : 0, "msg" : 0, "ch" : 0}
+        for channel in server.channels:
+            total["channels"] += 1
+            inCh = 0
+            msg = "```"
             try:
-                if server.get_channel(channel).server == server:
-                    message += "\n{} : {}".format(server.get_channel(channel).mention, self.channels[channel])
-            except AttributeError:
+                async for message in self.bot.logs_from(channel, limit=10000000, before=None):
+                    total["messages"] += 1
+                    if user and not message.author == user:
+                        continue
+                    if self.regex.match(message.content.lower()):
+                        total["msg"] += 1
+                        inCh += 1
+                        msg += "\n{} : {} >> {}".format(message.timestamp, message.id, message.author)
+                        await self.bot.add_reaction(message, "👎")
+                        if delete:
+                            try:
+                                await self.bot.delete_message(message)
+                            except discord.Forbidden:
+                                pass
+            except discord.Forbidden:
                 pass
-        await self.bot.say(message)
+            msg += "```"
+            if not msg == "``````":
+                total["ch"] += 1
+                await self.bot.send_message(ctx.message.channel, "Channel: {} Messages: `{}`\n{}".format(channel.mention, inCh, msg))
+        msg = "Done!"
+        msg += "\nScanned `{}` messages in `{}` channels in this servers.".format(total["messages"], total["channels"])
+        msg += "\nFound `{}` matches in `{}` channels.".format(total["msg"], total["ch"])
+        await self.bot.send_message(ctx.message.channel, msg)
+        
+
+    @dednuker.command(name="global", pass_context=True)
+    @checks.is_owner()
+    async def global_dednuker(self, ctx, delete: str = False, user: discord.User = None):
+        total = {"messages" : 0, "channels" : 0, "servers" : 0, "msg" : 0, "ch" : 0}
+        for server in self.bot.servers:
+            total["servers"] += 1
+            await self.bot.send_message(ctx.message.channel, "Server: `{}`".format(server.name))
+            for channel in server.channels:
+                total["channels"] += 1
+                inCh = 0
+                msg = "```"
+                try:
+                    async for message in self.bot.logs_from(channel, limit=10000000, before=None):
+                        total["messages"] += 1
+                        if user and not message.author == user:
+                            continue
+                        if self.regex.match(message.content.lower()):
+                            total["msg"] += 1
+                            inCh += 1
+                            msg += "\n{} : {} >> {}".format(message.timestamp, message.id, message.author)
+                            await self.bot.add_reaction(message, "👎")
+                except discord.Forbidden:
+                    pass
+                msg += "```"
+                if not msg == "``````":
+                    total["ch"] += 1
+                    await self.bot.send_message(ctx.message.channel, "Channel: {} Messages: `{}`\n{}".format(channel.mention, inCh, msg))
+        msg = "Done!"
+        msg += "\nScanned `{}` messages in `{}` channels in `{}` servers.".format(total["messages"], total["channels"], total["servers"])
+        msg += "\nFound `{}` matches in `{}` channels.".format(total["msg"], total["ch"])
+        await self.bot.send_message(ctx.message.channel, msg)
+        
+
+    
     async def on_message(self, message):
         """Message listener"""
-        if message.content is "ded":
+        if message.author.bot:
+            return
+        if self.regex.match(message.content.lower()):
+            await self.bot.add_reaction(message, "👎")
+            """overwrites = channel.overwrites_for(message.author)
+            overwrites.send_messages = False
+            try:
+                await self.bot.edit_channel_permissions(channel, user, overwrites)
+            except discord.Forbidden:"""
+            sleep(1)
             await self.bot.delete_message(message)
-    
-    def save(self):
-        dataIO.save_json('data/Antided/channels.json', self.channels)
-        dataIO.save_json('data/Antided/settings.json', self.settings)
-        
-def checks():
-    if not os.path.isdir("data/Antided"):
-        os.mkdir("data/Antided")
-    if not dataIO.is_valid_json("data/Antided/channels.json"):
-        dataIO.save_json("data/Antided/channels.json", {})
-    if not dataIO.is_valid_json("data/Antided/settings.json"):
-        dataIO.save_json("data/Antided/settings.json", {})
-        
+            """sleep(10)
+            overwrites.send_messages = False
+            try:
+                await self.bot.edit_channel_permissions(channel, user, overwrites)
+            except discord.Forbidden:
+                pass"""
+
 def setup(bot):
-    checks()
-    me = Antided(bot)
-    bot.add_listener(me.on_message,"on_message")
-    bot.add_cog(me)
+    bot.add_cog(Antided(bot))
