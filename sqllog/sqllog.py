86692d65ea6bf8ecb8e6016411f2b8d7052ac39a
Goofables <goofables@gmail.com>
2017-10-22 13:59:55 -0600

    Test database and config setup

---
+++ sqllog/sqllog.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+from .utils.dataIO import dataIO
+
+import PyMySQL
+
+class SQLlog:
+	"""Loggs messages to MySQL server"""
+	def __init__(self, bot):
+		self.bot = bot
+		self.login = dataIO.load_json("data/SQLlog/login.json")
+		self.db = PyMySQL.connect(self.login["host"],self.login["user"],_login["pass"],_login["db"])
+		cursor = db.cursor()
+		cursor.execute("SELECT VERSION()")
+		data = cursor.fetchone()
+		print("Version: {}".format(data))
+		print(db)
+		db.close()
+	
+	
+def fileCheck():
+	if not os.path.exists("data/SQLlog"):
+		os.mkdir("data/SQLlog")
+	if not os.path.isfile("data/SQLlog/login.json"):
+		dataIO.save_json("date/SQLlog/login.json", {"host":"localhost",
+													"user":"username",
+													"pass":"password",
+													"db":"DB"})
+
+def setup(bot):
+	fileCheck()
+	
+
+	
+	
+	bot.add_cog(SQLlog(bot))
+	
