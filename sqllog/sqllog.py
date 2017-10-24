b13a0a6a85614a50a1f1cf1200f7e6b5b5c766ae
Goofables <goofables@gmail.com>
2017-10-23 22:28:04 -0600

    New code

--- sqllog/sqllog.py
+++ sqllog/sqllog.py
@@ -1,34 +1,114 @@
 import discord
 from discord.ext import commands
 from .utils.dataIO import dataIO
+from .utils import checks
+import os
+import pymysql
 
-import PyMySQL
 
 class SQLlog:
 	"""Loggs messages to MySQL server"""
 	def __init__(self, bot):
 		self.bot = bot
-		self.login = dataIO.load_json("data/SQLlog/login.json")
-		self.db = PyMySQL.connect(self.login["host"],self.login["user"],_login["pass"],_login["db"])
-		cursor = db.cursor()
-		cursor.execute("SELECT VERSION()")
-		data = cursor.fetchone()
-		print("Version: {}".format(data))
-		print(db)
-		db.close()
 	
-	
+	async def on_message(self, message):
+		"""Message listener"""
+		await self.log_message(message)
+		
+	async def log_message(self, message):
+		query = """INSERT INTO `{0.channel.id}` (
+		`id`, `author.id`, `author.name`, `content`, `timestamp`, `type`, `attachments`, `embeds`
+		) VALUES (
+		'{0.id}', '{0.author.id}', '{1}', '{2}', '{0.timestamp}', '{0.type}', '{3}', '{4}'
+		)""".format(message,
+					message.author.name.replace("\\","\\\\").replace("'","\\'"),
+					message.content.replace("\\","\\\\").replace("'","\\'"),
+					str(message.attachments).replace("\\","\\\\").replace("'","\\'"),
+					str(message.embeds).replace("\\","\\\\").replace("'","\\'"),
+					)
+		try:
+			cursor.execute(query)
+		except UnicodeEncodeError:
+			print("Couldn't log message \"{}\"".format(message.content))
+			return
+		except pymysql.err.IntegrityError:
+			return
+		except pymysql.err.ProgrammingError as pe:
+			if not cursor.execute("SHOW TABLES LIKE '{}'".format(message.channel.id)):
+				print("Table `{0.id}` ({0.name}) does not exist! Creating now...".format(message.channel))
+				makedb = """CREATE TABLE `discordlog`.`{}` (
+					`id` BIGINT(18) NOT NULL ,
+					`author.id` BIGINT(18) NOT NULL ,
+					`author.name` TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+					`content` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+					`timestamp` TIMESTAMP NOT NULL ,
+					`type` TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+					`attachments` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,
+					`embeds` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,
+					PRIMARY KEY (`id`)) ENGINE = MyISAM;""".format(message.channel.id)
+				cursor.execute(makedb)
+				if cursor.execute("SHOW TABLES LIKE '{}'".format(message.channel.id)):
+					print("Success! Table `{0.id}` ({0.name}) created!".format(message.channel))
+					cursor.execute(query)
+				else:
+					print("Error! Table `{0.id}` ({0.name}) could not be created!".format(message.channel))
+					await self.bot.send_message(discord.utils.get(self.bot.get_all_members(), id=self.bot.settings.owner), "Error! Table `{0.id}` ({0.name}) could not be created!".format(message.channel))
+			else:
+				print("Error! Couldnt log message!")
+				print("`{0.timestamp}` > `{0.server.id}` > `{0.channel.id}` > > `{0.id}` > `{0.author}` > `{0.call}` > `{0.type}` > \"{0.content}\"".format(message))
+				await self.bot.send_message(discord.utils.get(self.bot.get_all_members(), id=self.bot.settings.owner), "`{0.timestamp}` > `{0.server.id}` > `{0.channel.id}` > > `{0.id}` > `{0.author}` > `{0.call}` > `{0.type}` > \"{0.content}\"".format(message))
+				print("ProgrammingError ({0}): {1}".format(pe.strerror))
+
+	@commands.command(pass_context=True)
+	@checks.is_owner()
+	async def logall(self, ctx):
+		await self.bot.say("Are you sure you want to log all messages to sql server?"
+						   " Type yes to confirm.")
+		response = await self.bot.wait_for_message(author=ctx.message.author)
+		if not response.content.lower().strip() == "yes":
+			await self.bot.say("Exiting.")
+			return
+		await self.log_message(ctx.message)
+		total = {"messages" : 0, "channels" : 0, "servers" : 0}
+		for server in self.bot.servers:
+			total["servers"] += 1
+			for channel in server.channels:
+				total["channels"] += 1
+				try:
+					async for message in self.bot.logs_from(channel, limit=10000000, before=None):
+						total["messages"] += 1
+						await self.log_message(message)
+				except discord.Forbidden:
+					pass
+		msg = "Done!"
+		msg += "\nScanned `{}` messages in `{}` channels in `{}` servers.".format(total["messages"], total["channels"], total["servers"])
+		await self.bot.send_message(ctx.message.channel, msg)
+		
 def fileCheck():
-	if not os.path.exists("data/SQLlog"):
-		os.mkdir("data/SQLlog")
-	if not os.path.isfile("data/SQLlog/login.json"):
-		dataIO.save_json("date/SQLlog/login.json", {"host":"localhost",
-													"user":"username",
-													"pass":"password",
-													"db":"DB"})
+	if not os.path.exists("data/sqllog"):
+		os.mkdir("data/sqllog")
+	if not os.path.isfile("data/sqllog/login.json"):
+		login = {"host" : "localhost", "user" : "username", "pass" : "password", "db" : "DB"}
+		dataIO.save_json("data/sqllog/login.json", login)
 
 def setup(bot):
 	fileCheck()
-	
+	global login
+	login = dataIO.load_json("data/sqllog/login.json")
+	try:
+		global db 
+		db = pymysql.connect(login["host"], login["user"], login["pass"], login["db"])
+		global cursor
+		cursor = db.cursor()
+		
+		cursor.execute("SELECT VERSION()")
+		data = cursor.fetchone()
+		
+		print("MySQL Version: {}".format(data))
+	except TypeError:
+		print ("Error! Could not login")
+		if db:
+			db.close()
+		return
 	bot.add_cog(SQLlog(bot))
 	
