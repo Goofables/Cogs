ac50be02bad984bb8cbbf448809eb80f6aa9a85a
Goofables <goofables@gmail.com>
2018-06-19 03:25:11 -0600

    Custom: working on nuke and speednuke

--- custom/custom.py
+++ custom/custom.py
@@ -80,7 +80,7 @@ class Custom:
 		await self.bot.edit_message(question, "Channel scanned. `{}` messages in nuke queue. Starting async nuke".format(n))
 		per = len(deleteList)//10
 		for i in range(10):
-			asyncio.ensure_future(delete(deleteList[i*per:(i+1)*per]))
+			asyncio.ensure_future(self.delete(deleteList[i*per:(i+1)*per]))
 				
 		await self.bot.say("Scanned channel `{}` messages".format(n))
 		await self.bot.delete_message(ctx.message)
