23e7b13732ed2b31f697ce9a2c97b53cc8d3799e
Goofables <goofables@gmail.com>
2018-06-20 03:36:16 -0600

    moved welcome messages to new cog Welcome

--- custom/custom.py
+++ custom/custom.py
@@ -12,10 +12,9 @@ import datetime
 import time
 
 class Custom:
-	"""Adds  usefull custom crap"""
+	"""Adds usefull custom crap"""
 	def __init__(self, bot):
 		self.bot = bot
-		self.channels = fileIO("data/custom/channels.json", "load")
 
 	def memform(self, num):
 		u = ""
@@ -36,7 +35,6 @@ class Custom:
 		else:
 			s = '%.3f'%s
 		return "{}{}".format(s, u)
-		
 	
 	@commands.command(pass_context=True)
 	@checks.is_owner()
@@ -246,80 +244,6 @@ class Custom:
 			await self.bot.delete_channel(created_channel)"""
 		except discord.errors.Forbidden:
 			await self.bot.say("Error")
-
-	async def member_join(self, member):
-		"""Member Join listener"""
-		server = member.server
-		print("<> Member '{}' joined server '{}'. {}".format(member.name, server.name, server.member_count))
-		for channel in self.channels:
-			try:
-				if self.channels[channel] and server.get_channel(channel).server == server:
-					await self.bot.send_message(server.get_channel(channel), ":small_red_triangle: Welcome {0} (#{2})".format(member.mention, server, server.member_count))
-			except AttributeError:
-				pass
-	   
-	async def member_remove(self, member):
-		"""Member remove listener"""
-		server = member.server
-		print("<> Member '{}' left server '{}'. {}".format(member.name, server.name, server.member_count))
-		for channel in self.channels:
-			try:
-				if self.channels[channel] and server.get_channel(channel).server == server:
-					await self.bot.send_message(server.get_channel(channel), ":small_red_triangle_down: Goodbye `{0}` (Members: {2})".format(member, server, server.member_count))
-			except AttributeError:
-				pass   
-
-	async def member_ban(self, member):
-		"""Member remove listener"""
-		server = member.server
-		print("<> Member '{}' banned from server '{}'. {}".format(member.name, server.name, server.member_count))
-		for channel in self.channels:
-			try:
-				if self.channels[channel] and server.get_channel(channel).server == server:
-					await self.bot.send_message(server.get_channel(channel), ":no_entry: `{0}` has been banned (Members: {2})".format(member, server, server.member_count))
-			except AttributeError:
-				pass
-			
-	@commands.command(pass_context=True, no_pm=True)
-	@checks.is_owner()
-	async def j(self, ctx, member: discord.Member):
-		"""Force member Join message"""
-		ctx = member
-		self.bot.dispatch("member_join", ctx)
-		self.bot.dispatch("member_remove", ctx)
-
-	@commands.group(pass_context=True, no_pm=True)
-	@checks.admin_or_permissions(manage_server=True)
-	async def welcome(self, ctx):
-		"""Manage channels for welcome messages"""
-		if ctx.invoked_subcommand is not None:
-			return
-		server = ctx.message.server
-		message = "Welcome channels:"
-		for channel in self.channels:
-			try:
-				if server.get_channel(channel).server == server:
-					message += "\n{} : {}".format(server.get_channel(channel).mention, self.channels[channel])
-			except AttributeError:
-				pass
-		await self.bot.say(message)
-		
-	@welcome.command(name="toggle", pass_context=True, no_pm=True)
-	async def toggle_welcome(self, ctx, channel: discord.Channel=None):
-		"""Toggles welcome messages for channel"""
-		if channel == None:
-			channel = ctx.message.channel
-		if channel.id not in self.channels:
-			self.channels[channel.id] = True
-		else:
-			self.channels[channel.id] = not self.channels[channel.id]
-		if self.channels[channel.id]:
-			await self.bot.say('Welcome enabled for {}'.format(channel.mention))
-		else:
-			await self.bot.say('Welcome disabled for {}'.format(channel.mention))
-			del self.channels[channel.id]
-		self.save_channels()
-		self.channels = fileIO("data/custom/channels.json", "load")
 		
 	@commands.command(pass_context=True)
 	@checks.is_owner()
@@ -373,12 +297,7 @@ class Custom:
 def checks():
 	if not os.path.exists("data/custom"):
 		os.mkdir("data/custom")
-	if not os.path.exists("data/custom/channels.json"):
-		fileIO("data/custom/channels.json", "save", {})
 		
 def setup(bot):
-	checks()
-	bot.add_listener(Custom(bot).member_join,"on_member_join")
-	bot.add_listener(Custom(bot).member_remove,"on_member_remove") 
-	bot.add_listener(Custom(bot).member_ban,"on_member_ban") 
+	#checks()
 	bot.add_cog(Custom(bot))
