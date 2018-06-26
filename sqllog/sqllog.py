20ccf95cef9fe21e798df2c17db4fa13e1ac2426
Goofables <goofables@gmail.com>
2018-06-25 23:09:15 -0600

    Working on sqllog. Tryinig to get log to work

--- sqllog/sqllog.py
+++ sqllog/sqllog.py
@@ -7,6 +7,7 @@ import os
 import pymysql
 from typing import Union
 import re
+from datetime import datetime,timedelta
 reg = re.compile("((\d+)d)?((\d+)h)?((\d+)m)?((\d+)s)?")
 
 class SQLlog:
@@ -18,7 +19,8 @@ class SQLlog:
     @checks.serverowner_or_permissions(administrator=True)
     async def log(self, ctx, time: str, amount: int = 10, channel: discord.Channel = None):
         time = reg.match(time)
-        await self.bot.say("Time: `{}` Amt: `{}` Ch: `{}`".format(time, amount, (channel, "all")[channel == None]))  
+        await self.bot.say("Time: `{}` Amt: `{}` Ch: `{}`".format(datetime.now() - timedelta(re.group), amount, (channel, "all")[channel == None]))  
+        await self.bot.say("{} {} {} {}".format(time.group(1), time.group(3), time.group(5), time.group(7)))
         ##status = await self.bot.say("Searching logs for messages from all users in {}.".format((channel.mention, "all channels")[channel == "*"]))
             
         cursor.execute("SELECT * FROM `` WHERE ``")
